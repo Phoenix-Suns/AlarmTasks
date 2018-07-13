dd25797ec910a599ec61dd18cf10a2e68ff304c2
Lưu Nghĩa Trần <luunghiatran@gmail.com>
2018-07-13 23:10:32 +0700

    [U] Animation Alarm Scene

--- app/src/main/java/com/windyroad/nghia/alarmtasks/helpers/Main2Activity.java
+++ app/src/main/java/com/windyroad/nghia/alarmtasks/helpers/Main2Activity.java
@@ -8,6 +8,9 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.CompoundButton;
 
 import com.windyroad.nghia.alarmtasks.R;
@@ -24,31 +27,12 @@ public class Main2Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        //now in the onCreate of your Activity you can do something like this:
-        List<String> permissionsToRequest = getPermissionsRequest(this, neededPermissions);
-        //if permissionsToRequest size is > 0 you need some more permission(s)
-        if (permissionsToRequest.size() > 0) {
-            ActivityCompat.requestPermissions(this,
-                    permissionsToRequest.toArray(new String[0]), PERMISSION_ALL_REQUEST_CODE);
-        }
+        View button = findViewById(R.id.button);
 
-        //now in the onRequestPermissionsResult you can handle everything
-        //as always and proceeed if everything is ok
+        // Animation
+        Animation anim = AnimationUtils.loadAnimation(Main2Activity.this, R.anim.slide_left);
+        button.startAnimation(anim);
     }
 
-    String[] neededPermissions = new String[]{
-            Manifest.permission.WAKE_LOCK,
-            Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE //if you can write, you can read
-    };
 
-    public static List<String> getPermissionsRequest(@NonNull Context context, @NonNull String[] permissions){
-        List<String> permissionsToRequest = new ArrayList<>();
-        for (int i = 0; i < permissions.length; i++){
-            if (ContextCompat.checkSelfPermission(context, permissions[i]) != PackageManager.PERMISSION_GRANTED){
-                permissionsToRequest.add(permissions[i]);
-            }
-        }
-        return permissionsToRequest;
-    }
 }
