743ae0b4f29c98dd7bfb4c73f4e0d75b0da2708d
Lưu Nghĩa Trần <luunghiatran@gmail.com>
2018-08-09 22:09:54 +0700

    a
    a

--- common/src/main/java/com/windyroad/nghia/common/activity/PermissionDialog.java
+++ common/src/main/java/com/windyroad/nghia/common/activity/PermissionDialog.java
@@ -44,51 +44,50 @@ private void requestVideoPermissions() {
 }*/
 public class PermissionDialog extends DialogFragment {
 
-private static final String ARG_MESSAGE = "message";
-private static final String ARG_PERMISSION = "permissions";
-private static final String ARG_REQUEST_CODE = "request_code";
+    private static final String ARG_MESSAGE = "message";
+    private static final String ARG_PERMISSION = "permissions";
+    private static final String ARG_REQUEST_CODE = "request_code";
 
-public static PermissionDialog newInstance(String message, String[] permissions, int requestCode) {
+    public static PermissionDialog newInstance(String message, String[] permissions, int requestCode) {
 
-    Bundle args = new Bundle();
+        Bundle args = new Bundle();
 
-    args.putString(ARG_MESSAGE, message);
-    args.putStringArray(ARG_PERMISSION, permissions);
-    args.putInt(ARG_REQUEST_CODE, requestCode);
+        args.putString(ARG_MESSAGE, message);
+        args.putStringArray(ARG_PERMISSION, permissions);
+        args.putInt(ARG_REQUEST_CODE, requestCode);
 
-    PermissionDialog fragment = new PermissionDialog();
-    fragment.setArguments(args);
-    return fragment;
-}
+        PermissionDialog fragment = new PermissionDialog();
+        fragment.setArguments(args);
+        return fragment;
+    }
 
-@Override
-public Dialog onCreateDialog(Bundle savedInstanceState) {
-    final String message = getArguments().getString(ARG_MESSAGE);
-    final String[] permissions = getArguments().getStringArray(ARG_PERMISSION);
-    final int requestCode = getArguments().getInt(ARG_REQUEST_CODE);
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final String message = getArguments().getString(ARG_MESSAGE);
+        final String[] permissions = getArguments().getStringArray(ARG_PERMISSION);
+        final int requestCode = getArguments().getInt(ARG_REQUEST_CODE);
 
-    final Fragment parent = getParentFragment();
-    AlertDialog alertDialog = new AlertDialog.Builder(getActivity())
-            .setMessage(message)
-            .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                        requestPermissions(permissions, requestCode);
-                    } else {
-                        FragmentCompat.requestPermissions(parent, permissions, requestCode);
-                    }
-                }
-            })
-            .setNegativeButton(android.R.string.cancel,
-                    new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            parent.getActivity().finish();
+        final Fragment parent = getParentFragment();
+        AlertDialog alertDialog = new AlertDialog.Builder(getActivity())
+                .setMessage(message)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                            requestPermissions(permissions, requestCode);
+                        } else {
+                            FragmentCompat.requestPermissions(parent, permissions, requestCode);
                         }
-                    })
-            .create();
-    return alertDialog;
-}
-
+                    }
+                })
+                .setNegativeButton(android.R.string.cancel,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                parent.getActivity().finish();
+                            }
+                        })
+                .create();
+        return alertDialog;
+    }
 }
